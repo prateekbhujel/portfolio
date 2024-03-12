bb01b60c3b7f8cbe579d4badecb897447a4b200d
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-03-12 14:59:35 +0545

    Update #40 --'Portfolio Section shown dynamically in frontend.'

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -7,6 +7,9 @@ use App\Models\Hero;
 use App\Models\Service;
 use App\Models\TyperTitle;
 use App\Models\About;
+use App\Models\Category;
+use App\Models\PortfolioSectionSetting as Setting;
+use App\Models\ProtfolioItem;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -14,12 +17,24 @@ class HomeController extends Controller
     
     public function index()
     {  
-        $hero         = Hero::first();
-        $typer_titles = TyperTitle::all();
-        $services     = Service::all();
-        $about        = About::first();
+        $hero                = Hero::first();
+        $typer_titles        = TyperTitle::all();
+        $services            = Service::all();
+        $about               = About::first();
+        $portfolioTitle      = Setting::first();
+        $portfolioCategories = Category::all();
+        $portfolioItems      = ProtfolioItem::all();
         
-       return view('frontend.home', compact('hero', 'typer_titles', 'services', 'about'));
+       return view('frontend.home', 
+              compact(
+                    'hero', 
+                    'typer_titles', 
+                    'services', 
+                    'about', 
+                    'portfolioTitle',
+                    'portfolioCategories',
+                    'portfolioItems'
+                ));
 
     }//End Method
 
